78adcc576ae24fef5b29935331293fee94f17dff
Kyle <kmstumpff@gmail.com>
2013-12-16 14:42:51 -0500

    Major Update
    Using object-oriented approach.

--- tinysong.py
+++ tinysong.py
@@ -1,7 +1,8 @@
+
 import urllib2
 import json
 
-API_KEY = '' #fill in with your key
+API_KEY = ''  # fill in with your key
 API_URL = 'http://tinysong.com/'
 API_FORMAT = 'json'
 global TS_ONE_RESULT
@@ -13,44 +14,64 @@ TS_MULTIPLE_WITH_METADATA = "s/"
 global TS_CHOICE
 TS_CHOICE = ""
 
-def get_title(ret, numb):
-    return ret[numb]['SongName']
 
-def get_songid(ret, numb):
-    return ret[numb]['SongID']
+class Query:
+    def __init__(self):
+        self.json = []
+        self.length = 0
 
-def get_artist(ret, numb):
-    return ret[numb]['ArtistName']
+    def check_results(self):
+        if self.json:
+            return True
+        else:
+            return False
 
-def get_album(ret, numb):
-    return ret[numb]['AlbumName']
+    def get_title(self, numb):
+        return self.json[numb]['SongName']
 
-def get_albumid(ret, numb):
-    return ret[numb]['AlbumID']
+    def get_songid(self, numb):
+        return self.json[numb]['SongID']
 
-def get_url(ret, numb):
-    return ret[numb]['Url']
 
-def api_call(query, ret_type, limit=5, parameters={}):
-    if ret_type == 1:
-        TS_CHOICE = TS_ONE_RESULT
-    if ret_type == 2:
-        TS_CHOICE = TS_ONE_WITH_METADATA
-    if ret_type == 3:
-        TS_CHOICE = TS_MULTIPLE_WITH_METADATA
-        C_LIMIT="&limit=" + str(limit)
-    else:
-        C_LIMIT=""
-    req = API_URL+TS_CHOICE+query.replace (" ", "+")+"?format="+API_FORMAT+"&key="+API_KEY+C_LIMIT
-    print("request: " + req)
-    response = urllib2.urlopen(req.lower()).read()
-    return json.loads(response)
+    def get_artist(self, numb):
+        return self.json[numb]['ArtistName']
+
+
+    def get_album(self, numb):
+        return self.json[numb]['AlbumName']
+
+
+    def get_albumid(self, numb):
+        return self.json[numb]['AlbumID']
+
+
+    def get_url(self, numb):
+        return self.json[numb]['Url']
+
+
+    def api_call(self, query, ret_type, limit=5):
+        if ret_type == 1:
+            TS_CHOICE = TS_ONE_RESULT
+        if ret_type == 2:
+            TS_CHOICE = TS_ONE_WITH_METADATA
+        if ret_type == 3:
+            TS_CHOICE = TS_MULTIPLE_WITH_METADATA
+            C_LIMIT="&limit=" + str(limit)
+        else:
+            TS_CHOICE = TS_MULTIPLE_WITH_METADATA
+            C_LIMIT=""
+        req = API_URL+TS_CHOICE+query.replace(" ", "+")+"?format="+API_FORMAT+"&key="+API_KEY+C_LIMIT
+        # print("request: " + req)
+        response = urllib2.urlopen(req.lower()).read()
+        self.json = json.loads(response)
+        self.length = len(self.json)
     
 
 class APIError(Exception):
     
     def __init__(self, value):
         self.message = value
+
     def __str__(self):
         return repr("There was a problem with your API request: " + self.message)
         
